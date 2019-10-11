4bd5e7648c617d857a0a3b33d5ce3140325bfd55
michael emmons <memmons18@gmail.com>
2019-10-11 07:07:22 -0400

    added router paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { ProductListComponent } from './product-list/product-list.component';
-
-
-
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { MainPageComponent } from "./main-page/main-page.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ListDetailComponent } from "./list-detail/list-detail.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -17,14 +16,20 @@ import { ProductListComponent } from './product-list/product-list.component';
     HeaderComponent,
     FooterComponent,
     MainPageComponent,
-    ProductListComponent
-    
+    ProductListComponent,
+    ListDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: "product-detail", component: ListDetailComponent },
+      { path: "welcome", component: MainPageComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full" }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
